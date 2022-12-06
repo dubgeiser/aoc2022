22ccd3fcbe2c27b71e69e0d3da73d53bb21de5b9
Per Juchtmans <per@dubgeiser.net>
2022-12-06 08:43:37 +0100

    Day 6, part 1

---
+++ 06/06.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+BUFSIZE = 4
+
+
+def read_data_file(fn):
+    with open(fn) as data:
+        stream = list(data.read().strip())
+    return stream
+
+
+def is_unique(buffer):
+    return len(buffer) == len(set(buffer))
+
+
+def solve(data):
+    i = 0
+    while True:
+        if is_unique(data[i:i + BUFSIZE]):
+            return i + BUFSIZE
+        i += 1
+
+
+if __name__ == "__main__":
+    answer = solve(read_data_file("input"))
+    print(f"{answer = }")
