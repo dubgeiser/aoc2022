65359be020e67bdb115d2e3037d55102bfb752ff
Per Juchtmans <per@dubgeiser.net>
2022-12-09 22:58:45 +0100

    Day 9, part 2 - slight clear up
    Undo the editing brainfart I committed in [f466286]

--- 09/2.py
+++ 09/2.py
@@ -9,24 +9,21 @@ def is_touching(p1: tuple[int, int], p2: tuple[int, int]) -> bool:
     return abs(p1[X] - p2[X]) <= 1 and abs(p1[Y] - p2[Y]) <= 1
 
 
-def follow(knots: list[tuple[int, int]]) -> None:
-    # We already moved knots[0]
-    for i in range(1, len(knots)):
-        head = knots[i - 1]
-        tail = knots[i]
-        if not is_touching(head, tail):
-            dx = dy = 0
+def follow(head: tuple[int, int], tail: tuple[int, int]) -> tuple[int, int]:
+    if not is_touching(head, tail):
+        dx = dy = 0
 
-            # Determine the movement the tail should take:
-            # The sign of the distance determines its movement along the axis, the
-            # division with its absolute value makes sure it's only 1, since we're\
-            # doing this step by step.
-            if head[X] != tail[X]:
-                dx = int((head[X] - tail[X]) / abs(head[X] - tail[X]))
-            if head[Y] != tail[Y]:
-                dy = int((head[Y] - tail[Y]) / abs(head[Y] - tail[Y]))
-            tail = (tail[X] + dx, tail[Y] + dy)
-        knots[i] = tail
+        # Determine the movement the tail should take:
+        # The sign of the distance determines its movement along the axis, the
+        # division with its absolute value makes sure it's only 1, since we're\
+        # doing this step by step.
+        if head[X] != tail[X]:
+            dx = int((head[X] - tail[X]) / abs(head[X] - tail[X]))
+        if head[Y] != tail[Y]:
+            dy = int((head[Y] - tail[Y]) / abs(head[Y] - tail[Y]))
+
+        tail = (tail[X] + dx, tail[Y] + dy)
+    return tail
 
 
 # Movement in (x, y) coordinates
@@ -34,6 +31,15 @@ movement = {'L': (-1, 0),
             'R': (1, 0),
             'U': (0, 1),
             'D': (0, -1)}
+
+
+def follow_the_leader(knots: list[tuple[int, int]], m: tuple[int,int]) -> None:
+    # Move head
+    knots[0] = (knots[0][X] + m[X], knots[0][Y] + m[Y])
+
+    # The rest follows in the same tail -> head way as in part 1
+    for i in range(1, len(knots)):
+        knots[i] = follow(knots[i - 1], knots[i])
 
 
 with open("input") as data:
@@ -45,8 +51,7 @@ for direction, distance in moves:
     distance = int(distance)
     m = movement[direction]
     for _ in range(distance):
-        knots[0] = (knots[0][X] + m[X], knots[0][Y] + m[Y])
-        follow(knots)
+        follow_the_leader(knots, m)
         visited.add(knots[-1])
 print()
 print(len(visited))
