2ac221abcf39efb3bb7deb42badb92a5a326f881
Per Juchtmans <per@dubgeiser.net>
2022-12-10 11:25:08 +0100

    Day 10, part 1

---
+++ 10/1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def is_important(cycle: int) -> bool:
+    return cycle in [20, 60, 100, 140, 180, 220]
+
+
+with open("input") as data:
+    program = [l.strip() for l in data]
+
+x = 1
+cycle = 0
+answer = 0
+
+for instruction in program:
+    cycle += 1
+    if instruction == "noop":
+        if is_important(cycle):
+            answer += cycle * x
+    else:
+        if is_important(cycle):
+            answer += cycle * x
+        cycle += 1
+        if is_important(cycle):
+            answer += cycle * x
+        v = int(instruction.replace("addx ", ""))
+        x += v
+
+print(f"{answer = }")
