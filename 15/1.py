f23d74ab14026b6f1f3038d672e670d1ccdc3086
Per Juchtmans <per@dubgeiser.net>
2022-12-25 02:34:23 +0100

    Day 15, part 1 optimization
    Part 1 ran really slow; _seconds_
    Basically going through all points on Y_TARGET and checking if it was
    covered was pretty brainless.
    Also remembered, finally, this "y_coverage" was in fact a list of
    'intervals'.
    By merging the overlapping parts and then summing their distances, the
    runtime was reduced to milliseconds instead of seconds.  This also opens
    up possibilities to kinda brute force part 2, I think.

--- 15/1.py
+++ 15/1.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python3
 
 import re
-import sys
-
-
-sample="""Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-Sensor at x=9, y=16: closest beacon is at x=10, y=16
-Sensor at x=13, y=2: closest beacon is at x=15, y=3
-Sensor at x=12, y=14: closest beacon is at x=10, y=16
-Sensor at x=10, y=20: closest beacon is at x=10, y=16
-Sensor at x=14, y=17: closest beacon is at x=10, y=16
-Sensor at x=8, y=7: closest beacon is at x=2, y=10
-Sensor at x=2, y=0: closest beacon is at x=2, y=10
-Sensor at x=0, y=11: closest beacon is at x=2, y=10
-Sensor at x=20, y=14: closest beacon is at x=25, y=17
-Sensor at x=17, y=20: closest beacon is at x=21, y=22
-Sensor at x=16, y=7: closest beacon is at x=15, y=3
-Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
 
 
 """
@@ -33,22 +16,34 @@ y = 10: dx: 2, 14   -> (sx - ?, sx + ?)
 """
 
 
+def merge_intervals(intervals):
+    x1 = 0
+    x2 = 1
+    last = -1
+    sorted_intervals = sorted(intervals)
+    merged = []
+    merged.append(sorted_intervals[0])
+    for i in sorted_intervals[1:]:
+        if merged[last][x1] <= i[x1] <= merged[last][x2]:
+            merged[last][x2] = max(merged[last][x2], i[x2])
+        else:
+            merged.append(i)
+    return merged
+
+
 regex_coord = r"x=(-?\d+), y=(-?\d+)"
 regex = re.compile(f"Sensor at {regex_coord}: closest beacon is at {regex_coord}")
 sensors = []
-beacons = []
 distances = []
 with open("input") as data:
     lines = [l.strip() for l in data]
     for l in lines:
         sx, sy, bx, by = map(int, regex.findall(l)[0])
         sensors.append((sx, sy))
-        beacons.append((bx, by))
         distances.append(abs(sx - bx) + abs(sy - by))
+
 Y_TARGET = 2000000
-y_coverage = []
-x_min = sys.maxsize
-x_max = -x_min
+intervals = []
 for i, s in enumerate(sensors):
     sx, sy = s
     dtarget = abs(sy - Y_TARGET)
@@ -59,23 +54,7 @@ for i, s in enumerate(sensors):
     dx = distances[i] - dtarget
     x_left = sx - dx
     x_right = sx + dx
-    x_min = min(x_left, x_min)
-    x_max = max(x_right, x_max)
-    y_coverage.append((x_left, x_right))
+    intervals.append([x_left, x_right])
 
-# following the puzzle example; beacons on Y_TARGET are valid positions to put
-# a beacon (Now that I'm writing this... UHU
-# Keep track of the beacons on Y_TARGET
-free_x = set([bx for bx, by in beacons if by == Y_TARGET])
-
-answer = 0
-# Go through outer most left side on Y_TARGET and outer most right side and
-# check if we have detected coverage on Y_TARGET (ie. a beacon cannot be present)
-for x in range(x_min, x_max + 1):
-    if x in free_x: continue # beacons on Y_TARGET
-    for xl, xr in y_coverage:
-        if x >= xl and x <= xr:
-            answer += 1
-            break
-
+answer = sum([abs(x2 - x1) for x1, x2 in merge_intervals(intervals)])
 print(f"{answer = }")
